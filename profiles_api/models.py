ed698229e00f06253ec7b3cc44b835c6fc002df3
nurtaims <nurtaims@gmail.com>
2019-08-28 11:37:00 +0600

    Add a user model manager

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with our custom user model."""
+    
+    def create_user(self, email, name, password=None):
+        """Creates a new profile user object."""
+        if not email:
+            raise ValueError('Users must have an email address.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details."""
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
