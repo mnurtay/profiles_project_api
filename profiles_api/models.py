b85f16a29fb8d649651e05248ef2a1236fa67800
nurtaims <nurtaims@gmail.com>
2019-09-11 23:09:33 +0600

    Added model for storing profile feed items, created migration and added to a django admin.

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
+
 class UserProfileManager(BaseUserManager):
     """Helps Django work with our custom user model."""
-    
+
     def create_user(self, email, name, password=None):
         """Creates a new profile user object."""
         if not email:
@@ -15,7 +16,7 @@ class UserProfileManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
+
     def create_superuser(self, email, name, password):
         """Creates and saves a new superuser with given details."""
         user = self.create_user(email, name, password)
@@ -38,9 +39,20 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def get_full_name(self):
         return self.name
-    
+
     def get_short_name(self):
         return self.name
-    
+
     def __str__(self):
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status update."""
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string."""
+        return self.status_text
